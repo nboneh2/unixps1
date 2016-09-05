c2a517d0a0d9f1dece798251af560651d4ee01d0
Nir Boneh <nboneh@localhost.localdomain>
2016-09-05 03:05:54 -0400

    Finish first script

--- script1.sh
+++ script1.sh
@@ -1,1 +1,19 @@
-df -hT / | tail -1 | tr -s   | cut -d   -f6 | sed s/%//
+#!/bin/bash
+
+#Requesting just the / directory, taking tail 1 to get rid of header line 
+#using trim to get rid of repeating spaces to make it suiteable for cut
+#cutting on spaces and choosing the 6 value which is the space used
+#remove the % sign with nothing using sed, so we can use the value as an int
+SPACEUSED="$(df -hT / | tail -1 | tr -s ' '  | cut -d ' ' -f6 | sed s/%//)"
+
+while [ $SPACEUSED -lt 80 ]
+do
+echo "Current spaceused: $SPACEUSED% (not critical)"
+sleep 1
+done
+
+#Sending mail to root
+echo "Current spaceused: $SPACEUSED% (critical) sending mail to root account"
+
+echo "Current space being used is $SPACEUSED%" | mail -s "Spaceused on system is critical" root 
+
